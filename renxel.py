a7f6a6148433278b7b8316a999759c5a2901d5aa
kagarisoft-dev <dev@kagarisoft.com>
2022-06-07 01:27:05 -0300

    update 3 files and delete 1 file

--- renxel.py
+++ renxel.py
@@ -26,7 +26,7 @@ import os
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
-
+import webbrowser as wb
 
 _version = "1.0.1"
 _debug_version = "1.0.2"
@@ -35,7 +35,7 @@ _debug_version = "1.0.2"
 class Window():
     def __init__(self, root):
         self.root = root
-        self.root.title("RenPy to Excel")
+        self.root.title("Renxel")
         self.rpyname = 'readonly'
         self.root.resizable(0, 0)
         self.root.configure(padx=10, pady=10)
@@ -91,13 +91,19 @@ class Window():
         self.root.destroy()
 
 
-def CheckVersion():
+def CheckVersion(root):
     try:
         result = requests.get(
             "https://api.github.com/repos/KagariSoft/renxel/releases/latest")
         version = result.json()["tag_name"]
-        if version > _version:
-            print("New version available: v{}".format(version))
+        if _debug_version > _version:
+            msg = messagebox.askokcancel(message="Renxel detected that there is a new version, do you want to update the app? This will open the Github.com page.",
+                                         title="New version available v{}".format(version))
+            if msg:
+                wb.open(
+                    "https://github.com/KagariSoft/renxel/releases/tag/{}".format(version))
+                root.destroy()
+
         else:
             print("No new version available")
 
@@ -112,7 +118,8 @@ def __main__():
         os.makedirs("out/rpy")
         os.makedirs("out/temp")
     root = tk.Tk()
-    CheckVersion()
+    root.title("Renxel")
+    CheckVersion(root)
     Window(root)
     root.mainloop()
 
