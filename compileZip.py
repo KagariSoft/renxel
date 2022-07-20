8d8b5e8ab7d1e48d8dc6bbb8e3dbc69cad2bf7c9
KagariSoft-Dev <dev@kagarisoft.com>
2022-07-20 17:29:54 -0300

    windows build

--- compileZip.py
+++ compileZip.py
@@ -1,7 +1,17 @@
 import zipfile
 import os
-renxel = "./dist/renxel"
-renxel_zip = "./dist/renxel-linux.zip"
-zip = zipfile.ZipFile(renxel_zip, "w", zipfile.ZIP_DEFLATED)
-zip.write(renxel, os.path.basename(renxel))
-zip.close()
+import platform
+if platform.platform() == "Windows":
+    renxel = "./dist/renxel.exe"
+    renxel_zip = "./dist/renxel-windows.zip"
+    zip = zipfile.ZipFile(renxel_zip, "w", zipfile.ZIP_DEFLATED)
+    zip.write(renxel, os.path.basename(renxel))
+    zip.close()
+elif platform.platform() == "Ubuntu":
+    renxel = "./dist/renxel"
+    renxel_zip = "./dist/renxel-linux.zip"
+    zip = zipfile.ZipFile(renxel_zip, "w", zipfile.ZIP_DEFLATED)
+    zip.write(renxel, os.path.basename(renxel))
+    zip.close()
+else:
+    print("I can't zip on this OS")
