c9c3a4a60e4659ec4ec08d3bd2956b6b22df9e6b
KagariSoft-Dev <dev@kagarisoft.com>
2023-03-15 14:06:11 -0300

    update build.sh and settings.set

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-pyinstaller --paths=lib --paths=assets --windowed --noconsole --clean --onefile --icon="window_icon.ico" --name="renxel" renxel.py
+pyinstaller --paths=./src/lib --paths=./src/lib/assets --paths=data --windowed --noconsole --clean --onefile --icon=./src/assets/window_icon.ico --add-data "<PythonLocation>/Lib/site-packages/customtkinter;customtkinter" --name=renxel ./src/main.py
 
 python3 ./compileZip.py
