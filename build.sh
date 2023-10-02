e15d2de7aae65a72e5267fd553801a9028850fdd
Neyunse <neyunse@kagarisoft.com>
2023-10-02 01:09:39 -0300

    update 1 file and delete 1 file

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 pyinstaller --paths=./src/lib --paths=./src/lib/assets --paths=data --windowed --noconsole --clean --onefile --icon=./src/assets/window_icon.ico --add-data "/home/<user>/.local/lib/python3.10/site-packages/customtkinter:customtkinter" --name=renxel ./src/main.py
-
-python3 ./compileZip.py
+ 
