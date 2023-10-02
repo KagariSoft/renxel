c5eb697646a9567d55c8ab07f7abd1ce8b5b0960
Neyunse <neyunse@kagarisoft.com>
2023-10-02 00:34:17 -0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-pyinstaller --paths=./src/lib --paths=./src/lib/assets --paths=data --windowed --noconsole --clean --onefile --icon=./src/assets/window_icon.ico --add-data "<PythonLocation>/Lib/site-packages/customtkinter;customtkinter" --name=renxel ./src/main.py
+pyinstaller --paths=./src/lib --paths=./src/lib/assets --paths=data --windowed --noconsole --clean --onefile --icon=./src/assets/window_icon.ico --add-data "/home/<user>/.local/lib/python3.10/site-packages/customtkinter:customtkinter" --name=renxel ./src/main.py
 
 python3 ./compileZip.py
